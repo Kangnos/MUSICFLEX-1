ee6e96a7441bfca9fabdfca8e2d57bf1df188d60
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-05-17 12:51:28 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -30,6 +30,22 @@ app.get('/Rap/:pageId', function(req, res) {
         })
     })
 })
+app.get('/Pop/:pageId', function(req, res) {
+    fs.readdir('./musics', function (err, filelist) {
+        var filteredMusic = path.parse(req.params.pageId).base;
+        fs.readFile(`musics/Pop/${filteredMusic}`, 'utf8', function (err, data) {
+            if( err ) throw err
+            var arr = data.split("\r\n")
+            var music_title = arr[0]
+            var videolinkcode = arr[1]
+            var nextlocation = arr[2]
+            fs.readFile('musics/Pop/PopMusics', 'utf8', function(err, musics){
+                var musicpage = MusicFlexMusicPage.HTML(music_title, videolinkcode, nextlocation, musics)
+                res.send(musicpage)
+            })
+        })
+    })
+})
 
 app.listen(3000, function() {
     console.log("Example app is running")
