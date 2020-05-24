9fee66611dffe208be0b3e8ceb587455a6b3e0c5
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-05-24 22:30:50 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var path = require('path')
 var MusicFlexMainPage = require('./lib/mainpage.js')
 var MusicFlexMusicPage = require('./lib/musicpage.js')
 var compression = require('compression');
+var url = require('url');
 app.use(express.static('public'));
 app.use(compression())
 
@@ -17,6 +18,7 @@ app.get('/', function(req, res){
 app.get('/Rap/:pageId', function(req, res) {
     fs.readdir('./musics', function (err, filelist) {
         var filteredMusic = path.parse(req.params.pageId).base;
+        var page_url = req.url;
         fs.readFile(`musics/Rap/${filteredMusic}`, 'utf8', function (err, data) {
             if( err ) throw err
             var arr = data.split("\r\n")
@@ -24,7 +26,7 @@ app.get('/Rap/:pageId', function(req, res) {
             var videolinkcode = arr[1]
             var nextlocation = arr[2]
             fs.readFile('musics/Rap/RapMusics', 'utf8', function(err, musics){
-                var musicpage = MusicFlexMusicPage.HTML(music_title, videolinkcode, nextlocation, musics)
+                var musicpage = MusicFlexMusicPage.HTML(music_title, videolinkcode, nextlocation, musics, page_url);
                 res.send(musicpage)
             })
         })
