009b71c44160f7bca77f3651ca4e0aa7e2c17841
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-05-24 22:36:01 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -10,38 +10,38 @@ app.use(express.static('public'));
 app.use(compression())
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     var mainpage = MusicFlexMainPage.HTML();
     res.send(mainpage)
 });
 
-app.get('/Rap/:pageId', function(req, res) {
+app.get('/Rap/:pageId', function (req, res) {
     fs.readdir('./musics', function (err, filelist) {
         var filteredMusic = path.parse(req.params.pageId).base;
         var page_url = req.url;
         fs.readFile(`musics/Rap/${filteredMusic}`, 'utf8', function (err, data) {
-            if( err ) throw err
+            if (err) throw err
             var arr = data.split("\r\n")
             var music_title = arr[0]
             var videolinkcode = arr[1]
             var nextlocation = arr[2]
-            fs.readFile('musics/Rap/RapMusics', 'utf8', function(err, musics){
+            fs.readFile('musics/Rap/RapMusics', 'utf8', function (err, musics) {
                 var musicpage = MusicFlexMusicPage.HTML(music_title, videolinkcode, nextlocation, musics, page_url);
                 res.send(musicpage)
             })
         })
     })
 })
-app.get('/Pop/:pageId', function(req, res) {
+app.get('/Pop/:pageId', function (req, res) {
     fs.readdir('./musics', function (err, filelist) {
         var filteredMusic = path.parse(req.params.pageId).base;
         fs.readFile(`musics/Pop/${filteredMusic}`, 'utf8', function (err, data) {
-            if( err ) throw err
+            if (err) throw err
             var arr = data.split("\r\n")
             var music_title = arr[0]
             var videolinkcode = arr[1]
             var nextlocation = arr[2]
-            fs.readFile('musics/Pop/PopMusics', 'utf8', function(err, musics){
+            fs.readFile('musics/Pop/PopMusics', 'utf8', function (err, musics) {
                 var musicpage = MusicFlexMusicPage.HTML(music_title, videolinkcode, nextlocation, musics)
                 res.send(musicpage)
             })
@@ -49,6 +49,6 @@ app.get('/Pop/:pageId', function(req, res) {
     })
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Example app is running")
 })
