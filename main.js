a6b29d94b6220da2812da0ba1cc4cf59cad93328
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-06-07 14:32:05 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -15,46 +15,16 @@ app.get('/', function (req, res) {
     res.send(mainpage)
 });
 
-app.get('/Rap/:pageId', function (req, res) {   
+app.get('/Rap', function (req, res) {   
     fs.readdir('./musics', function (err, filelist) {
-        var filteredMusic = path.parse(req.params.pageId).base;
-        var page_url = req.url;
-        var musictype = "Rap"
-        fs.readFile(`musics/${musictype}/${filteredMusic}`, 'utf8', function (err, data) {
-            if (err) throw err
-            var arr = data.split("\r\n")
-            var artist_name = arr[0]
-            var music_title = arr[1]
-            var videolinkcode = arr[2]
-            var nextmusic_title = arr[3]
-            var nextmusic_linkcode = arr[4]
-            fs.readFile('musics/Rap/RapMusics', 'utf8', function (err, musics) {
-                var musicpage = MusicFlexMusicPage.HTML(music_title, videolinkcode, musics, page_url, music_title, artist_name, musictype, nextmusic_title, nextmusic_linkcode);
-                res.send(musicpage)
-            })
+        fs.readFile('musics/Rap/RapMusics', 'utf8', function (err, musics) {
+            var musicpage = MusicFlexMusicPage.HTML(musics);
+            res.send(musicpage)
         })
     })
 })
 
-app.get('/Pop/:pageId', function (req, res) {
-    fs.readdir('./musics', function (err, filelist) {
-        var filteredMusic = path.parse(req.params.pageId).base;
-        var page_url = req.url;
-        var musictype = "Pop"
-        fs.readFile(`musics/${musictype}/${filteredMusic}`, 'utf8', function (err, data) {
-            if (err) throw err
-            var arr = data.split("\r\n")
-            var artist_name = arr[0]
-            var music_title = arr[1]
-            var videolinkcode = arr[2]
-            var nextmusic_title = arr[3]
-            fs.readFile('musics/Pop/PopMusics', 'utf8', function (err, musics) {
-                var musicpage = MusicFlexMusicPage.HTML(music_title, videolinkcode, musics, page_url, music_title, artist_name, musictype, nextmusic_title);
-                res.send(musicpage)
-            })
-        })
-    })
-})
+
 
 
 app.listen(3000, function () {
